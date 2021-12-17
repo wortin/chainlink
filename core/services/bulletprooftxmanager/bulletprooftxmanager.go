db79282ef916aa179e7b1152b0184e332ca797a4
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2021-12-17 14:03:59 -0400

    Applies gas fee to the balance check

--- core/services/bulletprooftxmanager/bulletprooftxmanager.go
+++ core/services/bulletprooftxmanager/bulletprooftxmanager.go
@@ -74,7 +74,7 @@ type TxManager interface {
 	CreateEthTransaction(newTx NewTx, qopts ...pg.QOpt) (etx EthTx, err error)
 	GetGasEstimator() gas.Estimator
 	RegisterResumeCallback(fn ResumeCallback)
-	SendEther(q pg.Q, chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error)
+	SendEther(chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error)
 }
 
 type BulletproofTxManager struct {
@@ -347,7 +347,7 @@ func (b *BulletproofTxManager) GetGasEstimator() gas.Estimator {
 }
 
 // SendEther creates a transaction that transfers the given value of ether
-func (b *BulletproofTxManager) SendEther(q pg.Q, chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error) {
+func (b *BulletproofTxManager) SendEther(chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error) {
 	if to == utils.ZeroAddress {
 		return etx, errors.New("cannot send ether to zero address")
 	}
@@ -363,7 +363,7 @@ func (b *BulletproofTxManager) SendEther(q pg.Q, chainID *big.Int, from, to comm
 	query := `INSERT INTO eth_txes (from_address, to_address, encoded_payload, value, gas_limit, state, evm_chain_id, created_at) VALUES (
 :from_address, :to_address, :encoded_payload, :value, :gas_limit, :state, :evm_chain_id, NOW()
 ) RETURNING eth_txes.*`
-	err = q.GetNamed(query, &etx, etx)
+	err = b.q.GetNamed(query, &etx, etx)
 	return etx, errors.Wrap(err, "SendEther failed to insert eth_tx")
 }
 
@@ -551,7 +551,7 @@ func (n *NullTxManager) Trigger(common.Address)                       { panic(n.
 func (n *NullTxManager) CreateEthTransaction(NewTx, ...pg.QOpt) (etx EthTx, err error) {
 	return etx, errors.New(n.ErrMsg)
 }
-func (n *NullTxManager) SendEther(q pg.Q, chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error) {
+func (n *NullTxManager) SendEther(chainID *big.Int, from, to common.Address, value assets.Eth, gasLimit uint64) (etx EthTx, err error) {
 	return etx, errors.New(n.ErrMsg)
 }
 func (n *NullTxManager) Healthy() error                           { return nil }
